26d667efc5f106a60957f1a53765526930dabbbb
Mahesh <mahesh.patil388@gmail.com>
2025-07-20 22:00:18 +0530

    same errorhandler views made

--- newproj/hello.py
+++ newproj/hello.py
@@ -1,5 +1,5 @@
-from flask import Flask,redirect,url_for,request,render_template
-
+from flask import Flask,redirect,url_for,request,render_template,make_response
+from markupsafe import escape
 app=Flask(__name__)
 
 
@@ -21,6 +21,22 @@ def login():
 def success(name):
     return f"Welcome, {name}!"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/user/<int:id>')
+def show_user_id(id):
+    # show the user profile for that user
+    return f'User {escape(id)}'
+
+@app.errorhandler(404)
+def not_found(error):
+    res=make_response(render_template('error.html'),404)
+    res.headers['X-headers']='Something '
+    return res
 
 
 if __name__=="__main__":
